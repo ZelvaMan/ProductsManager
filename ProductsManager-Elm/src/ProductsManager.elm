2dfde9eb8d2054c6e8f69f0b1da9ae1bcb19c44a
ZelvaMan <zelvaman16@gmail.com>
2020-01-28 20:20:00 +0100

    K http get add to elm

--- ProductsManager-Elm/src/ProductsManager.elm
+++ ProductsManager-Elm/src/ProductsManager.elm
@@ -6,11 +6,12 @@ module ProductsManager exposing (..)
 --   https://guide.elm-lang.org/architecture/buttons.html
 --
 
-
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
 import Bulma.Layout exposing (..)
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode
 
 
 
@@ -18,40 +19,117 @@ import Bulma.Layout exposing (..)
 
 
 main =
-  Browser.element { init = init, update = update, view = view }
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+--
+
+
+type Availability
+    = InCart
+    | Avalible
+    | UnAvailable
+
+
+type alias Product =
+    { id : Int
+    , name : String
+    , price : Int
+    , inStock : Int
+    , avail : Availability
+    }
 
 
 
 -- MODEL
 
 
-type alias Model = Int
+apiurl =
+    "localhost:51786/api"
+
+
+type alias Model =
+    Int
 
 
 init : Model
 init =
-  0
+    0
+
 
 type Msg
-  = LoadProducts     
+    = LoadProducts
+    | ProductsLoaded (Result Http.Error (List Product))
+
+
 
 -- UPDATE
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  --dodelat
-    model
+    --dodelat
+    case msg of
+        LoadProducts ->
+            handleLoadProducts model
 
 
 
 -- VIEW
 
 
+handleLoadProduct : Model -> ( Model, Cmd Msg )
+handleLoadProduct model =
+    ( model, LoadProducts )
+
+
+loadProducts : Cmd Msg
+loadProducts =
+    Http.get
+        { url = apiurl + "/Products"
+        , expect = Http.exeptJson ProductsLoaded getAllProductsResponseDecoder
+        }
+
+
+getAllProductsResponseDecoder : Decode.Decoder (List Product)
+getAllProductsResponseDecoder =
+    Decode.list Product productDecoder
+
+
+productDecoder : Decode.Decoder Product
+productDecoder =
+    decode Product
+        |> required "id" Decode.int
+        |> required "name" Decode.string
+        |> required "price" Decode.int
+        |> required "inStock" Decode.int
+        |> required "productAvailability" availabilityDecoder
+
+
+availabilityDecoder : Decode.Decoder Int -> Decode.Decoder Availability
+availabilityDecoder =
+    Decode.andThen
+        (\x ->
+            (case x of
+                0 ->
+                    InCart
+
+                1 ->
+                    Avalible
+
+                2 ->
+                    UnAvailable
+            )
+                |> decode.succeed
+        )
+
+
 view : Model -> Html Msg
-view model 
-   = main_ []
-    [
-    
-    
-    ]
+view model =
+    div []
+        []
